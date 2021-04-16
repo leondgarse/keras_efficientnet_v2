f1509579f69526600fea76fd9d49bb11b01195f8
leondgarse <leondgarse@gmail.com>
2021-04-16 14:53:58 +0800

    Add StochasticDepth

--- efficientnet_v2.py
+++ efficientnet_v2.py
@@ -80,7 +80,7 @@ def se_module(inputs, se_ratio=4):
     return Multiply()([inputs, se])
 
 
-def MBConv(inputs, output_channel, stride, expand_ratio, shortcut, use_se):
+def MBConv(inputs, output_channel, stride, expand_ratio, shortcut, survival=None, use_se=0):
     channel_axis = 1 if K.image_data_format() == "channels_first" else -1
     input_channel = inputs.shape[channel_axis]
     hidden_dim = input_channel * expand_ratio
@@ -106,31 +106,52 @@ def MBConv(inputs, output_channel, stride, expand_ratio, shortcut, use_se):
     nn = batchnorm_with_activation(nn, activation=None)
 
     if shortcut:
-        return Add()([inputs, nn])
+        if survival is not None and survival < 1:
+            from tensorflow_addons.layers import StochasticDepth
+
+            return StochasticDepth(float(survival))([inputs, nn])
+        else:
+            return Add()([inputs, nn])
     else:
         return nn
 
 
-def EfficientNetV2(input_shape=(224, 224, 3), include_top=True, classes=1000, width_mult=1, depth_mul=1, dropout=1, strides=2, name="EfficientNetV2"):
+def EfficientNetV2(input_shape=(224, 224, 3), include_top=True, classes=1000, width_mult=1, depth_mul=1, dropout=1, strides=2, survivals=None, name="EfficientNetV2"):
+    """
+    survivals is used for StochasticDepth. Can be a constant value like `0.5` or `0.8`,
+        or a tuple value like `(1, 0.5)` indicates the survival probability changes from `1 --> 0.5` for `top --> bottom` layers.
+        A higher value means a higher probability will keep the conv branch.
+    """
     inputs = Input(shape=input_shape)
     out_channel = _make_divisible(24 * width_mult, 8)
     nn = conv2d_no_bias(inputs, out_channel, (3, 3), strides=strides, padding="same")
     nn = batchnorm_with_activation(nn)
 
     expand_ratios = [1, 4, 4, 4, 6, 6]
-    output_channels = [24, 48, 64, 128, 160, 272]
+    out_channels = [24, 48, 64, 128, 160, 272]
     depths = [2, 4, 4, 6, 9, 15]
     strides = [1, 2, 2, 2, 1, 2]
     use_ses = [0, 0, 0, 1, 1, 1]
 
+    # StochasticDepth survival_probability values
+    depths = [int(math.ceil(ii * depth_mul)) for ii in depths]
+    total_layers = sum(depths)
+    if isinstance(survivals, float):
+        survivals = [survivals] * total_layers
+    elif isinstance(survivals, (list, tuple)) and len(survivals) == 2:
+        start, end = survivals
+        survivals = [start + (end - start) * ii / (total_layers - 1) for ii in range(total_layers)]
+    else:
+        survivals = [None] * total_layers
+    survivals = [survivals[int(sum(depths[:id])) : sum(depths[: id + 1])] for id in range(len(depths))]
+
     pre_out = out_channel
-    for expand_ratio, output_channel, depth, stride, se in zip(expand_ratios, output_channels, depths, strides, use_ses):
-        out = _make_divisible(output_channel * width_mult, 8)
-        depth = int(math.ceil(depth * depth_mul))
+    for expand_ratio, out_channel, depth, survival, stride, se in zip(expand_ratios, out_channels, depths, survivals, strides, use_ses):
+        out = _make_divisible(out_channel * width_mult, 8)
         for ii in range(depth):
             stride = stride if ii == 0 else 1
             shortcut = True if out == pre_out and stride == 1 else False
-            nn = MBConv(nn, out, stride, expand_ratio, shortcut, se)
+            nn = MBConv(nn, out, stride, expand_ratio, shortcut, survival[ii], se)
             pre_out = out
 
     out = _make_divisible(1792 * width_mult, 8)
