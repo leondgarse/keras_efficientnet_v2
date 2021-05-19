40e395d26d1531f050853dcfa52831f857831253
leondgarse <leondgarse@gmail.com>
2021-05-19 10:19:30 +0800

    Update archticture according to official

--- efficientnet_v2.py
+++ efficientnet_v2.py
@@ -24,8 +24,8 @@ from tensorflow.keras.layers import (
 )
 import math
 
-BATCH_NORM_DECAY = 0.99
-BATCH_NORM_EPSILON = 1e-5
+BATCH_NORM_DECAY = 0.9
+BATCH_NORM_EPSILON = 0.001
 CONV_KERNEL_INITIALIZER = keras.initializers.VarianceScaling(scale=2.0, mode="fan_out", distribution="truncated_normal")
 # CONV_KERNEL_INITIALIZER = 'glorot_uniform'
 
@@ -68,10 +68,14 @@ def batchnorm_with_activation(inputs, activation="swish"):
 
 def se_module(inputs, se_ratio=4):
     channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+    h_axis, w_axis = [2, 3] if K.image_data_format() == "channels_first" else [1, 2]
+
     filters = inputs.shape[channel_axis]
-    reduction = _make_divisible(filters // se_ratio, 8)
-    se = GlobalAveragePooling2D()(inputs)
-    se = Reshape((1, 1, filters))(se)
+    # reduction = _make_divisible(filters // se_ratio, 8)
+    reduction = filters // se_ratio
+    # se = GlobalAveragePooling2D()(inputs)
+    # se = Reshape((1, 1, filters))(se)
+    se = tf.reduce_mean(inputs, [h_axis, w_axis], keepdims=True)
     se = Conv2D(reduction, kernel_size=1, use_bias=True, kernel_initializer=CONV_KERNEL_INITIALIZER)(se)
     # se = PReLU(shared_axes=[1, 2])(se)
     se = Activation("swish")(se)
@@ -83,27 +87,33 @@ def se_module(inputs, se_ratio=4):
 def MBConv(inputs, output_channel, stride, expand_ratio, shortcut, survival=None, use_se=0):
     channel_axis = 1 if K.image_data_format() == "channels_first" else -1
     input_channel = inputs.shape[channel_axis]
-    hidden_dim = input_channel * expand_ratio
+    is_fused = use_se == 0
 
-    if use_se:
-        # pw
-        nn = conv2d_no_bias(inputs, hidden_dim, (1, 1), strides=(1, 1), padding="valid")
+    if is_fused and expand_ratio != 1:
+        nn = conv2d_no_bias(inputs, input_channel * expand_ratio, (3, 3), strides=stride, padding="same")
         nn = batchnorm_with_activation(nn)
-        # dw
+    elif expand_ratio != 1:
+        nn = conv2d_no_bias(inputs, input_channel * expand_ratio, (1, 1), strides=(1, 1), padding="same")
+        nn = batchnorm_with_activation(nn)
+    else:
+        nn = inputs
+
+    if not is_fused:
         nn = DepthwiseConv2D(
             (3, 3), padding="same", strides=stride, use_bias=False, depthwise_initializer=CONV_KERNEL_INITIALIZER
         )(nn)
         nn = batchnorm_with_activation(nn)
-        # se
+
+    if use_se:
         nn = se_module(nn, se_ratio=4 * expand_ratio)
-    else:
-        # fused, also se_ratio == 0
-        nn = conv2d_no_bias(inputs, hidden_dim, (3, 3), strides=stride, padding="same")
-        nn = batchnorm_with_activation(nn)
 
     # pw-linear
-    nn = conv2d_no_bias(nn, output_channel, (1, 1), strides=(1, 1), padding="valid")
-    nn = batchnorm_with_activation(nn, activation=None)
+    if is_fused and expand_ratio == 1:
+        nn = conv2d_no_bias(nn, output_channel, (3, 3), strides=stride, padding="same")
+        nn = batchnorm_with_activation(nn)
+    else:
+        nn = conv2d_no_bias(nn, output_channel, (1, 1), strides=(1, 1), padding="same")
+        nn = batchnorm_with_activation(nn, activation=None)
 
     if shortcut:
         if survival is not None and survival < 1:
@@ -117,14 +127,13 @@ def MBConv(inputs, output_channel, stride, expand_ratio, shortcut, survival=None
 
 
 def EfficientNetV2(
+    blocks_config,
     input_shape=(224, 224, 3),
-    include_top=True,
     classes=1000,
-    width_mul=1,
-    depth_mul=1,
-    dropout=1,
+    dropout=0.2,
     first_strides=2,
-    survivals=(1, 0.8),
+    survivals=None,
+    classifier_activation="softmax",
     name="EfficientNetV2",
 ):
     """
@@ -135,45 +144,101 @@ def EfficientNetV2(
         A higher value means a higher probability will keep the conv branch.
         or `None` to disable.
     """
+    expands = blocks_config["expands"]
+    out_channels = blocks_config["out_channels"]
+    depths = blocks_config["depths"]
+    strides = blocks_config["strides"]
+    use_ses = blocks_config["use_ses"]
+
     inputs = Input(shape=input_shape)
-    out_channel = _make_divisible(24 * width_mul, 8)
+    out_channel = _make_divisible(out_channels[0], 8)
     nn = conv2d_no_bias(inputs, out_channel, (3, 3), strides=first_strides, padding="same")
     nn = batchnorm_with_activation(nn)
 
-    expands = [1, 4, 4, 4, 6, 6]
-    out_channels = [24, 48, 64, 128, 160, 272]
-    depths = [2, 4, 4, 6, 9, 15]
-    strides = [1, 2, 2, 2, 1, 2]
-    use_ses = [0, 0, 0, 1, 1, 1]
-
     # StochasticDepth survival_probability values
-    depths = [int(math.ceil(ii * depth_mul)) for ii in depths]
     total_layers = sum(depths)
     if isinstance(survivals, float):
         survivals = [survivals] * total_layers
     elif isinstance(survivals, (list, tuple)) and len(survivals) == 2:
         start, end = survivals
-        survivals = [start + (end - start) * ii / (total_layers - 1) for ii in range(total_layers)]
+        survivals = [start - (1 - end) * float(ii) / total_layers for ii in range(total_layers)]
     else:
         survivals = [None] * total_layers
     survivals = [survivals[int(sum(depths[:id])) : sum(depths[: id + 1])] for id in range(len(depths))]
 
     pre_out = out_channel
     for expand, out_channel, depth, survival, stride, se in zip(expands, out_channels, depths, survivals, strides, use_ses):
-        out = _make_divisible(out_channel * width_mul, 8)
+        out = _make_divisible(out_channel, 8)
         for ii in range(depth):
             stride = stride if ii == 0 else 1
             shortcut = True if out == pre_out and stride == 1 else False
             nn = MBConv(nn, out, stride, expand, shortcut, survival[ii], se)
             pre_out = out
 
-    out = _make_divisible(1792 * width_mul, 8)
+    out = _make_divisible(1280, 8)
     nn = conv2d_no_bias(nn, out, (1, 1), strides=(1, 1), padding="valid")
     nn = batchnorm_with_activation(nn)
 
-    if include_top:
+    if classes > 0:
         nn = GlobalAveragePooling2D(name="avg_pool")(nn)
         if dropout > 0 and dropout < 1:
             nn = Dropout(dropout)(nn)
-        nn = Dense(classes, activation="softmax", name="predictions")(nn)
+        nn = Dense(classes, activation=classifier_activation, name="predictions")(nn)
     return Model(inputs=inputs, outputs=nn, name=name)
+
+
+def EfficientNetV2S(
+    input_shape=(224, 224, 3),
+    classes=1000,
+    dropout=0.2,
+    first_strides=2,
+    survivals=None,
+    classifier_activation="softmax",
+    name="EfficientNetV2S",
+):
+    blocks_config = {
+        "expands": [1, 4, 4, 4, 6, 6],
+        "out_channels": [24, 48, 64, 128, 160, 256],
+        "depths": [2, 4, 4, 6, 9, 15],
+        "strides": [1, 2, 2, 2, 1, 2],
+        "use_ses": [0, 0, 0, 1, 1, 1],
+    }
+    return EfficientNetV2(**locals())
+
+
+def EfficientNetV2M(
+    input_shape=(224, 224, 3),
+    classes=1000,
+    dropout=0.2,
+    first_strides=2,
+    survivals=None,
+    classifier_activation="softmax",
+    name="EfficientNetV2S",
+):
+    blocks_config = {
+        "expands": [1, 4, 4, 4, 6, 6, 6],
+        "out_channels": [24, 48, 80, 160, 176, 304, 512],
+        "depths": [3, 5, 5, 7, 14, 18, 5],
+        "strides": [1, 2, 2, 2, 1, 2, 1],
+        "use_ses": [0, 0, 0, 1, 1, 1, 1],
+    }
+    return EfficientNetV2(**locals())
+
+
+def EfficientNetV2L(
+    input_shape=(224, 224, 3),
+    classes=1000,
+    dropout=0.2,
+    first_strides=2,
+    survivals=None,
+    classifier_activation="softmax",
+    name="EfficientNetV2S",
+):
+    blocks_config = {
+        "expands": [1, 4, 4, 4, 6, 6, 6],
+        "out_channels": [32, 64, 96, 192, 224, 384, 640],
+        "depths": [4, 7, 7, 10, 19, 25, 7],
+        "strides": [1, 2, 2, 2, 1, 2, 1],
+        "use_ses": [0, 0, 0, 1, 1, 1, 1],
+    }
+    return EfficientNetV2(**locals())
