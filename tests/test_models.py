d203438e0a7bd2f46f5c4460b0d6d9512bc029ac
leondgarse <leondgarse@aol.com>
2021-08-24 16:48:24 +0800

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -10,11 +10,10 @@ import keras_efficientnet_v2
 
 
 def test_model_predict_t_imagenet():
-    """Run a single forward pass with each model"""
+    """ Run a single forward pass with EfficientNetV2T on imagenet """
     model = keras_efficientnet_v2.EfficientNetV2T(pretrained="imagenet")
     imm = tf.image.resize(chelsea(), model.input_shape[1:3])  # Chelsea the cat
     pred = model(tf.expand_dims(imm / 255, 0)).numpy()
-    print(keras.applications.imagenet_utils.decode_predictions(pred)[0])
     out = keras.applications.imagenet_utils.decode_predictions(pred)[0][0]
 
     assert out[1] == "Egyptian_cat"
@@ -22,11 +21,10 @@ def test_model_predict_t_imagenet():
 
 
 def test_model_predict_b3_imagenet21k_ft1k():
-    """Run a single forward pass with each model"""
+    """ Run a single forward pass with EfficientNetV2B3 on imagenet21k-ft1k """
     model = keras_efficientnet_v2.EfficientNetV2B3(pretrained="imagenet21k-ft1k")
     imm = tf.image.resize(chelsea(), model.input_shape[1:3])  # Chelsea the cat
     pred = model(tf.expand_dims(imm / 255, 0)).numpy()
-    print(keras.applications.imagenet_utils.decode_predictions(pred)[0])
     out = keras.applications.imagenet_utils.decode_predictions(pred)[0][0]
 
     assert out[1] == "Egyptian_cat"
@@ -34,7 +32,7 @@ def test_model_predict_b3_imagenet21k_ft1k():
 
 
 def test_model_predict_s_imagenet21k():
-    """Run a single forward pass with each model"""
+    """ Run a single forward pass with EfficientNetV2S on imagenet21k """
     model = keras_efficientnet_v2.EfficientNetV2S(num_classes=21843, pretrained="imagenet21k")
     imm = tf.image.resize(chelsea(), model.input_shape[1:3])  # Chelsea the cat
     pred = model(tf.expand_dims(imm / 255, 0)).numpy()
